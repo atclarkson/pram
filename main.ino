0cebd92f3f13c1a5cbd34f3917183314e5484ae9
Adam <atclarkson@gmail.com>
2019-05-04 09:52:32 -0400

    Initial Commit

---
+++ main.ino
@@ -0,0 +1,16 @@
+/*    File: main.ino
+ *    Name: Adam T Clarkson
+ *    Date: May 4 2019
+ *    Course: CSCI 201 Computer Science II
+ *    Desc: Program for Stack M5 Dev Unit to control two relays (outputs) based on distance pulses input.
+ */
+
+ #include <M5Stack.h>
+
+ void setup() {
+
+ }
+
+ void loop() {
+   
+ }
