d57d7520a3f7905ee07141f4602f6b503600f09e
Adam <atclarkson@gmail.com>
2019-05-04 18:30:11 -0400

    Underscores and Arrows for enter num menu

--- main/settings.cpp
+++ main/settings.cpp
@@ -35,20 +35,36 @@ void setRelay2() {
 }
 
 int enterNum(int value) {
-  String printValue;
+  String printValue = "";
   if (value > 9999) {
     printValue = value;
   } else if (value > 999) {
-    printValue =  String("0" + value);
+    printValue =  String("0" + (String)value);
   } else if (value > 99) {
-    printValue =  String("00" + value);
+    printValue =  String("00" + (String)value);
   } else if (value > 9) {
-    printValue =  String("000" + value);
+    printValue =  String("000" + (String)value);
   } else {
-    printValue =  String("0000" + value);
+    printValue =  String("0000" + (String)value);
   }
   M5.Lcd.setTextColor(TFT_BLACK);
   int curser = 15;
   M5.Lcd.drawCentreString(printValue, 160, 40, 7);
-  drawButtons("<", WHITE, BLACK, "^", WHITE, BLACK , "SAVE", WHITE, GREEN);
+
+
+  int y = 90;
+  int w = 29;
+  int h = 6;
+  M5.Lcd.fillRect(0, y, 320, h, WHITE);
+  M5.Lcd.fillRect(81, y, w, h, GREEN);
+  M5.Lcd.fillRect(113, y, w, h, RED);
+  M5.Lcd.fillRect(145, y, w, h, BLUE);
+  M5.Lcd.fillRect(177, y, w, h, ORANGE);
+  M5.Lcd.fillRect(209, y, w, h, BLACK);
+  drawButtons("", WHITE, BLACK, "", WHITE, BLACK , "SAVE", WHITE, GREEN);
+  // left arrow
+  M5.Lcd.fillTriangle(43,213,62,202,62,224,WHITE);
+  // up arrow
+  M5.Lcd.fillTriangle(149,223,171,223,160,204,WHITE);
+  return value;
 }
