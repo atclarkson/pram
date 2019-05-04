d7a7ef78ad24fe6b269188b94960c3b792531034
Adam <atclarkson@gmail.com>
2019-05-04 10:00:46 -0400

    Add setup routine

--- main.ino
+++ main.ino
@@ -5,12 +5,43 @@
  *    Desc: Program for Stack M5 Dev Unit to control two relays (outputs) based on distance pulses input.
  */
 
- #include <M5Stack.h>
+#include <M5Stack.h>
 
- void setup() {
+// Global Variables - Pin Locations
+const byte relayPin1 = 5;
+const byte relayPin2 = 13;
+const byte interruptPin = 17;  // Distance Pulses
+const byte buttonA = 39;
+const byte buttonB = 38;
+const byte buttonC = 37;
+const byte speaker = 25;
 
- }
+void setup() {
+  // Open Serial Port
+  Serial.begin(9600);
+  // Start M5
+  M5.begin();
+  // Setup the sceen
+  M5.Lcd.fillScreen(TFT_BLACK);
+  M5.Lcd.fillScreen(WHITE);
+  M5.Lcd.setTextColor(TFT_BLACK);
+  // Draw button images
+  M5.Lcd.fillRoundRect(223,194,85,39,4,RED);
+  M5.Lcd.drawCentreString("Reset", 265, 205, 4); // Draw text centre at position 120, 30 using font 4
 
- void loop() {
-   
- }
+  // Set pin mode for output
+  pinMode(relayPin1, OUTPUT);
+  pinMode(relayPin2, OUTPUT);
+  // Bring the relay pins high by default
+  digitalWrite(relayPin1, HIGH);
+  digitalWrite(relayPin2, HIGH);
+  // Setup Interupts
+  // Interupt for speed signal
+  attachInterrupt(digitalPinToInterrupt(interruptPin), countSpeed, RISING);
+  // Interupt for Button C
+  attachInterrupt(digitalPinToInterrupt(37), resetCounter, CHANGE);  
+}
+
+void loop() {
+
+}
